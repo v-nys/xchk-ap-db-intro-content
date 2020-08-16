537a545a84acf4d8bdc0d15cbddfac88f2c6d768
Vincent Nys <vincentnys@gmail.com>
2020-08-16 17:03:54 +0200

    Foutje mbt namespace

--- xchk_ap_db_intro_content/course.py
+++ xchk_ap_db_intro_content/course.py
@@ -1,6 +1,6 @@
 from xchk_core import contentviews as basecv
 from xchk_core.courses import Course
-from xchk_docker_content import *
+from xchk_docker_content.contentviews import *
 from xchk_mysql_content.contentviews import *
 from .contentviews import *
 
