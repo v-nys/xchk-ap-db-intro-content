8459b6adcd512610d9fb47634095e04b5c022ccb
Vincent Nys <vincentnys@gmail.com>
2020-09-19 15:44:52 +0200

    Gekoppeld aan strats.

--- xchk_ap_db_intro_content/contentviews.py
+++ xchk_ap_db_intro_content/contentviews.py
@@ -1,19 +1,18 @@
 from xchk_core.contentviews import ContentView
 from xchk_core.strats import *
+from xchk_ap_db_intro_content.strats import *
 
 class DatatypesSimpeleTekstView(ContentView):
     uid = 'xchk_ap_db_intro_content_datatypes_simpele_tekst'
     title = 'datatypes: simpele tekst'
     template = 'xchk_ap_db_intro_content/datatypes_simpele_tekst.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
+    strat = xchk_ap_db_intro_content_datatypes_simpele_tekst_strat
 
 class BasisstructurenVanEenRelationeleDatabaseView(ContentView):
     uid = 'xchk_ap_db_intro_content_basisstructuren_van_een_relationele_database'
     title = 'Basisstructuren van een relationele database'
     template = 'xchk_ap_db_intro_content/basisstructuren_van_een_relationele_database.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
+    strat = xchk_ap_db_intro_content_basisstructuren_van_een_relationele_database_strat
 
 class LokaleVsRemoteDatabaseView(ContentView):
     uid = 'xchk_ap_db_intro_content_lokale_vs_remote_database'
@@ -26,14 +25,14 @@ class VoordelenVanEenDatabaseView(ContentView):
     uid = 'xchk_ap_db_intro_content_voordelen_van_een_database'
     title = 'Voordelen van een database'
     template = 'xchk_ap_db_intro_content/voordelen_van_een_database.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    strat = xchk_ap_db_intro_content_voordelen_van_een_database_strat
 
 
 class SubtalenVanMySqlView(ContentView):
     uid = 'xchk_ap_db_intro_content_subtalen_van_my_sql'
     title = 'subtalen van (My)SQL'
     template = 'xchk_ap_db_intro_content/subtalen_van_my_sql.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    strat = xchk_ap_db_intro_content_subtalen_van_my_sql_strat
 
 
 class SchermovernameView(ContentView):
@@ -47,8 +46,7 @@ class DiagramnotatieTabelView(ContentView):
     uid = 'xchk_ap_db_intro_content_diagramnotatie_tabel'
     title = 'diagramnotatie tabel'
     template = 'xchk_ap_db_intro_content/diagramnotatie_tabel.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
+    strat = xchk_ap_db_intro_content_diagramnotatie_tabel_strat
 
 class InstallatieDockerView(ContentView):
     uid = 'xchk_ap_db_intro_content_installatie_docker'
@@ -68,7 +66,7 @@ class WatIsEenDatabankView(ContentView):
     uid = 'xchk_ap_db_intro_content_wat_is_een_databank'
     title = 'Wat is een databank'
     template = 'xchk_ap_db_intro_content/wat_is_een_databank.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    strat = xchk_ap_db_intro_content_wat_is_een_databank_strat
 
 
 class VerbindenMetEenLokaleDatabaseServerViaWorkbenchView(ContentView):
@@ -82,8 +80,7 @@ class CreateStatementView(ContentView):
     uid = 'xchk_ap_db_intro_content_create_statement'
     title = 'CREATE statement'
     template = 'xchk_ap_db_intro_content/create_statement.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
+    strat = xchk_ap_db_intro_content_create_statement_strat
 
 class MysqlDockerImageView(ContentView):
     uid = 'xchk_ap_db_intro_content_mysql_docker_image'
@@ -96,15 +93,13 @@ class DatatypesGeheleGetallenView(ContentView):
     uid = 'xchk_ap_db_intro_content_datatypes_gehele_getallen'
     title = 'datatypes: gehele getallen'
     template = 'xchk_ap_db_intro_content/datatypes_gehele_getallen.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
+    strat = xchk_ap_db_intro_content_datatypes_gehele_getallen_strat
 
 class WatIsDockerView(ContentView):
     uid = 'xchk_ap_db_intro_content_wat_is_docker'
     title = 'Wat is Docker?'
     template = 'xchk_ap_db_intro_content/wat_is_docker.html'
     strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
 
 class VerbindenMetDeDatabaseServerViaWorkbenchView(ContentView):
     uid = 'xchk_ap_db_intro_content_verbinden_met_de_database_server_via_workbench'
@@ -117,18 +112,17 @@ class WatIsMysqlView(ContentView):
     uid = 'xchk_ap_db_intro_content_wat_is_mysql'
     title = 'Wat is MySQL?'
     template = 'xchk_ap_db_intro_content/wat_is_mysql.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    strat = Strategy(refusing_check=Negation(TrueCheck()),accepting_check=TrueCheck())
 
 
 class WatIsEenRelationeleDatabaseView(ContentView):
     uid = 'xchk_ap_db_intro_content_wat_is_een_relationele_database'
     title = 'Wat is een relationele database?'
     template = 'xchk_ap_db_intro_content/wat_is_een_relationele_database.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
-
+    strat = xchk_ap_db_intro_content_wat_is_een_relationele_database_strat
 
 class DatabaseSelecterenView(ContentView):
     uid = 'xchk_ap_db_intro_content_database_selecteren'
     title = 'database selecteren'
     template = 'xchk_ap_db_intro_content/database_selecteren.html'
-    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    strat = Strategy(refusing_check=Negation(TrueCheck()),accepting_check=TrueCheck())
