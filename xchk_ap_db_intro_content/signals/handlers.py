420614d40c5d6a296c848c73b4c99265aee92a3b
Vincent Nys <vincentnys@gmail.com>
2020-09-29 14:21:59 +0200

    log statements...

--- xchk_ap_db_intro_content/signals/handlers.py
+++ xchk_ap_db_intro_content/signals/handlers.py
@@ -7,8 +7,11 @@ import mysql.connector
 
 @receiver(pre_save)
 def create_shared_db_user(sender, instance, **kwargs):
+    print("pre_save handler")
     if sender is Repo:
+        print("repo gemaakt")
         if instance.course == 'databankenintro':
+            print("repo DBI gemaakt")
             mydb = mysql.connector.connect(host='studentmysql',user='root',password=settings.STUDENTMYSQL_ROOT_PW)
             mycursor = mydb.cursor()
             mycursor.execute(f'create database {user.username};')
