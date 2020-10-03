a752ed9d7e610982d301544bddea6a55fde2e94d
Vincent Nys <vincentnys@gmail.com>
2020-10-03 15:37:31 +0200

    Moet kunnen zien...

--- xchk_ap_db_intro_content/course.py
+++ xchk_ap_db_intro_content/course.py
@@ -3,4 +3,19 @@ from xchk_core import contentviews as basecv
 from xchk_core.courses import Course
 from .contentviews import *
 
-course = Course('databankenintro','Databanken Intro',[(BasisstructurenVanEenRelationeleDatabaseView,[WatIsEenRelationeleDatabaseView]),(CreateStatementView,[DiagramnotatieTabelView,DatabaseSelecterenView]),(DatabaseSelecterenView,[SubtalenVanMySqlView]),(DatatypesGeheleGetallenView,[SubtalenVanMySqlView]),(DatatypesSimpeleTekstView,[SubtalenVanMySqlView]),(DiagramnotatieTabelView,[DatatypesSimpeleTekstView,DatatypesGeheleGetallenView]),(LokaleVsRemoteDatabaseView,[VerbindenMetDeDatabaseServerViaWorkbenchView]),(MysqlWorkbenchInstallerenView,[WatIsMysqlView]),(SchermovernameView,[]),(VoorbeeldinstructiesMySQLView,[WatIsMysqlView]),(SubtalenVanMySqlView,[VoorbeeldinstructiesMySQLView]),(VerbindenMetDeDatabaseServerViaWorkbenchView,[MysqlWorkbenchInstallerenView]),(VoordelenVanEenDatabaseView,[WatIsEenDatabankView]),(WatIsEenDatabankView,[]),(WatIsEenRelationeleDatabaseView,[VoordelenVanEenDatabaseView]),(WatIsMysqlView,[BasisstructurenVanEenRelationeleDatabaseView])])
+course = Course('databankenintro','Databanken Intro',[(BasisstructurenVanEenRelationeleDatabaseView,[WatIsEenRelationeleDatabaseView]),
+                                                      (DropStatementView,[CreateStatementView]),
+                                                      (CreateStatementView,[DiagramnotatieTabelView,DatabaseSelecterenView]),
+                                                      (DatabaseSelecterenView,[SubtalenVanMySqlView]),
+                                                      (DatatypesGeheleGetallenView,[SubtalenVanMySqlView]),
+                                                      (DatatypesSimpeleTekstView,[SubtalenVanMySqlView]),
+                                                      (DiagramnotatieTabelView,[DatatypesSimpeleTekstView,DatatypesGeheleGetallenView]),
+                                                      (LokaleVsRemoteDatabaseView,[VerbindenMetDeDatabaseServerViaWorkbenchView]),
+                                                      (MysqlWorkbenchInstallerenView,[WatIsMysqlView]),
+                                                      (VoorbeeldinstructiesMySQLView,[WatIsMysqlView]),
+                                                      (SubtalenVanMySqlView,[VoorbeeldinstructiesMySQLView]),
+                                                      (VerbindenMetDeDatabaseServerViaWorkbenchView,[MysqlWorkbenchInstallerenView]),
+                                                      (VoordelenVanEenDatabaseView,[WatIsEenDatabankView]),
+                                                      (WatIsEenDatabankView,[]),
+                                                      (WatIsEenRelationeleDatabaseView,[VoordelenVanEenDatabaseView]),
+                                                      (WatIsMysqlView,[BasisstructurenVanEenRelationeleDatabaseView])])
