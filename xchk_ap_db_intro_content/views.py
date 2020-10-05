b224ff93e273f15e1f58f79d23bcbd071f440fef
Vincent Nys <vincentnys@gmail.com>
2020-10-05 07:48:19 +0200

    testen

--- xchk_ap_db_intro_content/views.py
+++ xchk_ap_db_intro_content/views.py
@@ -6,11 +6,15 @@ import mysql.connector
 # Create your views here.
 def create_dbs_view(request):
     for db_repo in Repo.objects.filter(course='databankenintro'):
-        uname = db_repo.user.username
-        upw = db_repo.user.initial_pw
-        mydb = mysql.connector.connect(host='studentmysql',database='sys',user='root',password=settings.STUDENTMYSQL_ROOT_PASSWORD)
-        mycursor = mydb.cursor()
-        mycursor.execute(f'create database if not exists {uname};')
-        mycursor.execute(f"create user if not exists {uname} identified by '{upw}';")
-        mycursor.execute(f"grant all on {uname}.* to {uname}@'%';")
-        mycursor.close()
+        try:
+            uname = db_repo.user.username
+            upw = db_repo.user.initial_pw
+            mydb = mysql.connector.connect(host='studentmysql',database='sys',user='root',password=settings.STUDENTMYSQL_ROOT_PASSWORD)
+            mycursor = mydb.cursor()
+            mycursor.execute(f'create database if not exists {uname};')
+            mycursor.execute(f"create user if not exists {uname} identified by '{upw}';")
+            mycursor.execute(f"grant all on {uname}.* to {uname}@'%';")
+            mycursor.close()
+        except Exception as e:
+            print(e)
+    return render(request,'xchk_ap_db_intro_content/created.html',{})
